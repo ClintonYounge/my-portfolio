c1b93e789a66cbbb3bf573305bf8fba07497687e
ClintonYounge <clintonyounge18@gmail.com>
2023-11-07 18:46:18 -0400

    Update App.js to render projects

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import './responsive-desktop.css';
+import projects from './components/state';
+import projImg from './images/ExpGy1.jpeg';
+import dotShape from './images/Dot-shape.png';
+import ProjectPopup from './components/projectPopup';
 
 function App() {
+  const [openProject, setOpenProject] = useState(null);
+
+  const handleProjectClick = (project) => {
+    setOpenProject(project);
+  };
+
+  const handleClosePopup = () => {
+    setOpenProject(null);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section id="work-section">
+      <div className="container">
+        {projects.map((project, index) => (
+          <div className="projects" key={index}>
+            <img src={projImg} alt="snapshot of project 1" className="snapshot" />
+
+            <div className="snapshot-details">
+              <div className="works-description">
+                <div className="works-intro">
+                  <h2 className="project-title gy">{project.name}</h2>
+                  <div className="frame">
+                    <span className="client">CANOPY</span>
+                    <img src={dotShape} alt="" className="counter" />
+                    <span className="role">{project.role}</span>
+                    <img src={dotShape} alt="" className="counter" />
+                    <span className="year">{project.year}</span>
+                  </div>
+                </div>
+              </div>
+
+              <p className="details">{project.details}</p>
+
+              <ul className="tags">
+                {project.tags.map((tag, tagIndex) => (
+                  <li key={tagIndex}>{tag}</li>
+                ))}
+              </ul>
+
+              <button
+                className="action-button see-project"
+                onClick={() => handleProjectClick(project)}
+              >
+                See Project
+              </button>
+            </div>
+          </div>
+        ))}
+
+        {openProject && (
+          <ProjectPopup project={openProject} onClose={handleClosePopup} />
+        )}
+      </div>
+    </section>
   );
 }
 
