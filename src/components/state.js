3a4593062eda81e8265b40166c4fc650b99d586a
ClintonYounge <clintonyounge18@gmail.com>
2023-11-07 18:46:32 -0400

    Add projects to state

---
+++ src/components/state.js
@@ -0,0 +1,60 @@
+const projects = [
+  {
+    id: 0,
+    name: 'Doctor Appointments Backend',
+    role: 'Back End Dev',
+    year: '2023',
+    details: 'A ruby on rails backend for a react/redux front end app that allows users to signup, login, and interact with doctors and make appointments.',
+    tags: ['Ruby on Rails', 'PostgreSQL']
+  },
+  {
+    id: 1,
+    name: 'Catalog of Things Ruby Capstone',
+    role: 'Back End Dev',
+    year: '2023',
+    details: 'A console app that will help you to keep a record of different types of things you own: books, music albums, movies, and games',
+    tags: ['Ruby on Rails', 'PostgreSQL']
+  },
+  {
+    id: 2,
+    name: 'Meals Budgeting App',
+    role: 'Front End Dev',
+    year: '2023',
+    details: 'A mobile web application where you can manage your budget to spend on meals: you have a list of orders associated with a meal, so that you can see how much money you spent and on what.',
+    tags: ['HTML5', 'Ruby on Rails', 'CSS3']
+  },
+  {
+    id: 3,
+    name: 'Space Traveller',
+    role: 'Front End Dev',
+    year: '2023',
+    details: 'In this project, I collaborated with another developer and worked with live data from the SpaceX API. We built a web application for a company that provides commercial and scientific space travel services. The application will allow users to book rockets and join space missions.',
+    tags: ['React', 'Redux', 'JavaScript', 'CSS3']
+  },
+  {
+    id: 4,
+    name: 'Mobile Tech Financial Digest',
+    role: 'Front End Dev',
+    year: '2023',
+    details: 'Mobile Tech Financial Digest is a react redux project which allows users to go through financial data of Apple, Samsung, Xiaomi and Google by interacting.',
+    tags: ['React', 'Redux', 'JavaScript', 'CSS3']
+  },
+  {
+    id: 5,
+    name: 'JS Capstone Group Project',
+    role: 'Full Stack Dev',
+    year: '2023',
+    details: 'A JavaScript capstone project about building a web application based on an external API. In a group with 2 other programmers, we selected the Meals API that provides data about meals then built the webapp around it.',
+    tags: ['HTML5', 'CSS3', 'JavaScript']
+  },
+  {
+    id: 6,
+    name: 'Experience Guyana',
+    role: 'Front End Dev',
+    year: '2023',
+    details: 'A chance to expeience the country Guyana. An event filled tour awaits.',
+    tags: ['HTML5', 'CSS3', 'JavaScript']
+  },
+]
+
+module.exports = projects;
